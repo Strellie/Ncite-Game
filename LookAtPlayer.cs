12d7419195e529c9fb4338905cf585757eee73ff
Strellie <LukaszStrellie@gmail.com>
2016-08-16 17:08:27 -0700

    Add files via upload

---
+++ LookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+	public Transform player;
+	public Transform ojectToFollow;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = ojectToFollow.position;
+		transform.LookAt(player);
+	}
+}
