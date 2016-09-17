7cf3124d1bbaa78c44c30c17e0c91c1ac17801a9
Strellie <LukaszStrellie@gmail.com>
2016-09-16 20:12:27 -0700

    Update Companion.cs

--- Companion.cs
+++ Companion.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Companion : MonoBehaviour {
@@ -6,19 +6,25 @@ public class Companion : MonoBehaviour {
 	public Transform player;
 	public Transform lookAtPlayer;
 
+	public Transform wayPoint;
+
+	public GameObject rightWayPoint;
+	public GameObject leftWayPoint;
+
 	public float speed = 10;
 	public Rigidbody rb;
 
 	public Timer timer;
-	public Timer timer2;
+
+	public bool rightStep = false;
+	public bool leftStep = false;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag("Player").GetComponent<Transform>();
 		rb = GetComponent<Rigidbody>();
-		//timer.GetComponent<Timer>();
 		timer = new Timer();
-		timer2 = new Timer();
+		//timer.GetComponent<Timer>()
 	}
 	
 	// Update is called once per frame
@@ -28,21 +34,57 @@ public class Companion : MonoBehaviour {
 		//Quaternion rotation = Quaternion.identity;
 		//rotation.eulerAngles = new Vector3(0, 0, lookAtPlayer.rotation.y);
 
-		timer2.CountDown(20,4);
-		if(timer.CountUp(0,4))
-			timer.ResetTimer();
-		//timer.GetCurrentTime;
-		print(timer.ToString());
-		print(timer2.ToString());
 
 		Vector3 movement = transform.forward * speed * Time.deltaTime;
-		if(Vector3.Distance(transform.position, player.position) > 4)
+		if(/*wayPoint != null*/ rightStep ){
+			if(timer.CountDown(0.2F)){
+				rightStep = false;
+				timer.ResetTimer();
+			}
+			//transform.position = Vector3.Lerp(transform.position, wayPoint.position, 0.05F);
+			//transform.position = Vector3.Lerp(transform.position, transform.position, 0.05F);
+			gameObject.GetComponent<Rigidbody>().AddForce(transform.right * 20);
+			//transform.LookAt(wayPoint);
+			//transform.rotation = Quaternion.RotateTowards(transform.rotation, lookAtPlayer.rotation, 100*Time.deltaTime);
+		}else if(/*wayPoint != null*/ leftStep ){
+			if(timer.CountDown(0.2F)){
+				leftStep = false;
+				timer.ResetTimer();
+			}
+			gameObject.GetComponent<Rigidbody>().AddForce(transform.right * -20);
+		}else if(Vector3.Distance(transform.position, player.position) > 4 && wayPoint == null){
 			//rb.MovePosition(transform.position + movement);
 			transform.position = Vector3.Lerp(transform.position, transform.position + movement, 0.5F);
 		//transform.Rotate(Vector3.up, Time.deltaTime * 200);
 		//transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 0.5F);
+		}
 		transform.rotation = Quaternion.RotateTowards(transform.rotation, lookAtPlayer.rotation, 100*Time.deltaTime);
+
+		if(wayPoint!=null && Vector3.Distance(transform.position, wayPoint.position)<= 0.1){
+			//Destroy(wayPoint.gameObject);
+			wayPoint = null;
+		}
 		//transform.LookAt(player);
 	
 	}
+
+	void OnTriggerEnter(Collider collider){
+
+		Companion otherAI = collider.gameObject.GetComponentInParent<Companion>();
+		if(collider.gameObject.tag == "Enemy" /*&& wayPoint == null*/){
+			float rightDis = Vector3.Distance(transform.position, otherAI.rightWayPoint.transform.position);
+			float leftDis = Vector3.Distance(transform.position, otherAI.leftWayPoint.transform.position);
+			if(rightDis < leftDis){
+				rightStep = true;
+				//wayPoint = otherAI.rightWayPoint.transform;
+				//wayPoint = Instantiate(otherAI.rightWayPoint.transform, otherAI.rightWayPoint.transform.position, transform.rotation) as Transform;
+			}else{
+				leftStep = true;
+				//wayPoint = otherAI.leftWayPoint.transform;
+				//wayPoint = Instantiate(otherAI.leftWayPoint.transform, otherAI.leftWayPoint.transform.position, transform.rotation) as Transform;
+			}
+		}
+
+	}
+
 }
